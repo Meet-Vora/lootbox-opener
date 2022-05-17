e0b166e350e09d4496e2492616cc00f6735c7119
Meet Vora <meetrajivvora@gmail.com>
2022-05-17 15:11:17 -0700

    minor patch update, v1.1.2 rollout

--- lootbox_opener.py
+++ lootbox_opener.py
@@ -2,10 +2,10 @@ import mouse
 import sys
 from time import sleep
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 2:
     raise Exception("must pass in number of lootboxes to open")
 
-num_lootboxes = sys.argv[2]
+num_lootboxes = int(sys.argv[1])
 counter = 0
 x_coord = 1711
 y_coord = 1315
@@ -13,9 +13,9 @@ y_coord = 1315
 # wait for you to double click left click, and then starts the program
 mouse.wait(button='right', target_types='double')
 mouse.move(x_coord, y_coord, absolute=True)
-while(counter < num_lootboxes)
+while(counter < num_lootboxes):
     mouse.click(button='left')
-    sleep(5)
+    sleep(5.5)
     counter += 1
 # print(mouse.get_position())
 
